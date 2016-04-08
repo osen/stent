44fc17de5ff9c505e172a4f1f11fd094d8c7dd52
osen <kpedersen@live.co.uk>
2016-04-08 13:44:18 +0000

    Additional string functions.

--- stentext.c
+++ stentext.c
@@ -1,6 +1,7 @@
 #include "stentext.h"
 
 #include <string.h>
+#include <stdio.h>
 
 struct String
 {
@@ -25,6 +26,38 @@ REF(String) StringAllocCStr(char *str)
   return rtn;
 }
 
+void StringCopy(REF(String) dest, REF(String) src)
+{
+  GET(dest)->length = GET(src)->length;
+  free(GET(dest)->data);
+  GET(dest)->data = strdup(GET(src)->data);
+}
+
+void StringAdd(REF(String) ctx, REF(String) add)
+{
+  GET(ctx)->length += GET(add)->length;
+  GET(ctx)->data = realloc(GET(ctx)->data, sizeof(char) * (GET(ctx)->length + 1));
+  strcat(GET(ctx)->data, GET(add)->data);
+}
+
+void StringAddCStr(REF(String) ctx, char *add)
+{
+  GET(ctx)->length += strlen(add);
+  GET(ctx)->data = realloc(GET(ctx)->data, sizeof(char) * (GET(ctx)->length + 1));
+  strcat(GET(ctx)->data, add);
+}
+
+void StringAddInt(REF(String) ctx, int add)
+{
+  // TODO: Get correct size
+  char str[15] = {};
+
+  sprintf(str, "%d", add);
+  GET(ctx)->length += strlen(str);
+  GET(ctx)->data = realloc(GET(ctx)->data, sizeof(char) * (GET(ctx)->length + 1));
+  strcat(GET(ctx)->data, str);
+}
+
 void StringFree(REF(String) ctx)
 {
   free(GET(ctx)->data);
@@ -34,4 +67,122 @@ void StringFree(REF(String) ctx)
 char *StringCStr(REF(String) ctx)
 {
   return GET(ctx)->data;
+}
+
+void StringAddChar(REF(String) ctx, char c)
+{
+  GET(ctx)->data = realloc(GET(ctx)->data, sizeof(char) * (GET(ctx)->length + 2));
+  GET(ctx)->length++;
+  GET(ctx)->data[GET(ctx)->length - 1] = c;
+  GET(ctx)->data[GET(ctx)->length] = '\0';
+}
+
+void StringClear(REF(String) ctx)
+{
+  GET(ctx)->length = 0;
+  GET(ctx)->data[0] = '\0';
+}
+
+size_t StringLength(REF(String) ctx)
+{
+  return GET(ctx)->length;
+}
+
+struct InputFile
+{
+  FILE *file;
+  REF(String) buffer;
+  int eof;
+};
+
+static void InputFileUpdateBuffer(REF(InputFile) ctx)
+{
+  char buffer[128] = {};
+  char *res = NULL;
+  size_t len = 0;
+  int i = 0;
+
+  StringClear(GET(ctx)->buffer);
+
+  while(1)
+  {
+    res = fgets(buffer, sizeof(buffer), GET(ctx)->file);
+
+    if(res == NULL)
+    {
+      if(StringLength(GET(ctx)->buffer) == 0)
+      {
+        StringFree(GET(ctx)->buffer);
+      }
+
+      return;
+    }
+
+    len = strlen(res);
+
+    for(i = 0; i < len; i++)
+    {
+      if(buffer[i] == '\n' || buffer[i] == '\r')
+      {
+        return;
+      }
+
+      StringAddChar(GET(ctx)->buffer, buffer[i]);
+    }
+  }
+}
+
+REF(InputFile) InputFileOpen(char *path)
+{
+  REF(InputFile) rtn = {};
+
+  rtn = CALLOC(InputFile);
+
+  GET(rtn)->file = fopen(path, "r");
+
+  if(GET(rtn)->file == NULL)
+  {
+    FREE(rtn);
+
+    return rtn;
+  }
+
+  GET(rtn)->buffer = StringAllocCStr("");
+  InputFileUpdateBuffer(rtn);
+
+  return rtn;
+}
+
+void InputFileClose(REF(InputFile) ctx)
+{
+  fclose(GET(ctx)->file);
+
+  if(GET(GET(ctx)->buffer) != NULL)
+  {
+    StringFree(GET(ctx)->buffer);
+  }
+
+  FREE(ctx);
+}
+
+int InputFileEof(REF(InputFile) ctx)
+{
+  if(GET(GET(ctx)->buffer) == NULL)
+  {
+    return 1;
+  }
+
+  return 0;
+}
+
+void InputFileReadLine(REF(InputFile) ctx, REF(String) out)
+{
+  if(InputFileEof(ctx))
+  {
+    printf("No data left in buffer\n");
+    abort();
+  }
+
+  StringCopy(out, GET(ctx)->buffer);
+  InputFileUpdateBuffer(ctx);
 }
--- stentext.h
+++ stentext.h
@@ -8,5 +8,19 @@ REFDEF(String);
 REF(String) StringAllocCStr(char *str);
 void StringFree(REF(String) ctx);
 char *StringCStr(REF(String) ctx);
+void StringAddChar(REF(String) ctx, char c);
+void StringClear(REF(String) ctx);
+void StringCopy(REF(String) dest, REF(String) src);
+void StringAdd(REF(String) ctx, REF(String) add);
+size_t StringLength(REF(String) ctx);
+void StringAddCStr(REF(String) ctx, char *add);
+void StringAddInt(REF(String) ctx, int add);
+
+REFDEF(InputFile);
+
+REF(InputFile) InputFileOpen(char *path);
+void InputFileClose(REF(InputFile) ctx);
+int InputFileEof(REF(InputFile) ctx);
+void InputFileReadLine(REF(InputFile) ctx, REF(String) out);
 
 #endif
