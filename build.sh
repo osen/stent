dd3120511e2445ae9058802c3e63771f54e8ffb6
osen <kpedersen@live.co.uk>
2016-06-01 11:36:16 +0100

    Added test for exception transactions. Also provides good test for userData destroyed in nested function. TODO: Finalizers should be called in reverse order of creation.

--- build.sh
+++ build.sh
@@ -8,7 +8,8 @@ cc -Wall -obin/game -I. example/game/*.c stent.c
 cc -Wall -obin/leak -I. example/leak/*.c stent.c
 cc -Wall -obin/array -I. example/array/*.c stent.c
 cc -Wall -obin/cast -I. example/cast/*.c stent.c
-cc -g -Wall -obin/exception -I. example/exception/*.c stent.c
+cc -Wall -obin/exception -I. example/exception/*.c stent.c
+cc -Wall -obin/transaction -I. example/transaction/*.c stent.c
 cc -Wall -obin/string -I. example/string/*.c *.c
 
 #cc -Wall `pkg-config --cflags gtk+-3.0` -I. -obin/gtk example/gtk/*.c *.c `pkg-config --libs gtk+-3.0`
---
+++ example/transaction/main.c
@@ -0,0 +1,84 @@
+#include <stent.h>
+
+REFDEF(First);
+REFDEF(Second);
+REFDEF(Third);
+REFDEF(UserData);
+
+struct UserData
+{
+  int a;
+};
+
+struct First
+{
+  int a;
+};
+
+struct Second
+{
+  char b;
+};
+
+struct Third
+{
+  float c;
+};
+
+void FirstFinalizer(REF(First) ctx)
+{
+  printf("~First\n");
+}
+
+void SecondFinalizer(REF(Second) ctx)
+{
+  printf("~Second\n");
+}
+
+void ThirdFinalizer(REF(Third) ctx)
+{
+  printf("~Third\n");
+}
+
+void safe_main(REF(UserData) userData)
+{
+  REF(First) first = {};
+  REF(Second) second = {};
+  REF(Third) third = {};
+
+  first = CALLOC(First);
+  FINALIZER(first, FirstFinalizer);
+
+  FREE(userData);
+
+  second = CALLOC(Second);
+  FINALIZER(second, SecondFinalizer);
+
+  third = CALLOC(Third);
+  FINALIZER(third, ThirdFinalizer);
+
+  THROW(0, "Something exceptional has happened");
+}
+
+int main(int argc, char *argv[])
+{
+  REF(Exception) exception = {};
+  REF(UserData) userData = {};
+
+  userData = CALLOC(UserData);
+  exception = TRY(safe_main, userData);
+
+  if(TRYGET(exception) != NULL)
+  {
+    printf("%s\n", GET(exception)->message);
+    FREE(exception);
+  }
+
+  //printf("User Data: %p\n", GET(userData));
+  //FREE(userData);
+
+  StentStats();
+  StentCleanup();
+
+  return 0;
+}
